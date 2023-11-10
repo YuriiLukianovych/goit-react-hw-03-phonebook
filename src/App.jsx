742a31e66ba2ae80d01dd28de8dacc1883f0d780
Yurii Lukianovych <mr.lukyanovich@gmail.com>
2023-11-10 02:03:59 +0200

    .jkhfdgfhcvjkjhjvhc

--- src/App.jsx
+++ src/App.jsx
@@ -69,7 +69,7 @@ export default class App extends Component {
   componentDidMount() {
     const contacts = JSON.parse(localStorage.getItem('contacts'));
 
-    if (contacts & contacts.length) {
+    if (contacts) {
       this.setState({
         contacts,
       });
